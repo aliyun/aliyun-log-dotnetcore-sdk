f53457a66540926b296866f4eccd101383436c6d
wb-cjh663673 <wb-cjh663673@alibaba-inc.com>
2021-01-13 15:08:04 +0800

    urlEncode

--- Aliyun.Api.LogService/Infrastructure/Protocol/Http/HttpRequestMessageBuilder.cs
+++ Aliyun.Api.LogService/Infrastructure/Protocol/Http/HttpRequestMessageBuilder.cs
@@ -32,6 +32,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
+using System.Web;
 using Aliyun.Api.LogService.Infrastructure.Authentication;
 using Aliyun.Api.LogService.Utils;
 using Google.Protobuf;
@@ -366,24 +367,16 @@ namespace Aliyun.Api.LogService.Infrastructure.Protocol.Http
 
         #endregion Signature
 
-
         public HttpRequestMessage Build()
         {
             // Validate
             Ensure.NotNull(this.credential, nameof(this.credential));
             Ensure.NotEmpty(this.credential.AccessKeyId, nameof(this.credential.AccessKeyId));
             Ensure.NotEmpty(this.credential.AccessKey, nameof(this.credential.AccessKey));
-           
-            // Rebuild the RequestUri
-            var queryString = String.Join("&", this.query
-                .OrderBy(x => x.Key)
-                .Select(x => $"{x.Key}={x.Value}"));
-            var pathAndQuery = queryString.IsNotEmpty() ? $"{this.path}?{queryString}" : this.path;
-            this.httpRequestMessage.RequestUri = new Uri(pathAndQuery, UriKind.Relative);
 
             // Process sts-token.
             var hasSecurityToken = this.httpRequestMessage.Headers.TryGetValues(LogHeaders.SecurityToken, out var securityTokens)
-                && securityTokens.FirstOrDefault().IsNotEmpty();
+                                   && securityTokens.FirstOrDefault().IsNotEmpty();
 
             if (!hasSecurityToken && this.credential.StsToken.IsNotEmpty())
             {
@@ -437,7 +430,24 @@ namespace Aliyun.Api.LogService.Infrastructure.Protocol.Http
             var signature = Convert.ToBase64String(this.ComputeSignature());
             this.httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue("LOG", $"{this.credential.AccessKeyId}:{signature}");
 
+            // Rebuild the RequestUri
+            var queryString = String.Join("&", this.query
+                .OrderBy(x => x.Key)
+                .Select(x => $"{encodeUrl(x.Key)}={encodeUrl(x.Value)}"));
+            var pathAndQuery = queryString.IsNotEmpty() ? $"{this.path}?{queryString}" : this.path;
+            this.httpRequestMessage.RequestUri = new Uri(pathAndQuery, UriKind.Relative);
+            
             return this.httpRequestMessage;
+        }
+
+        private String encodeUrl(String value)
+        {
+            if (value==null)
+            {
+                return "";
+            }
+            string encoded = HttpUtility.UrlEncode(value, this.encoding);
+            return encoded.Replace("+", "%20").Replace("*", "%2A").Replace("~", "%7E").Replace("/", "%2F");
         }
     }
 }
